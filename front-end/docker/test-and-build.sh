fcec836503c58691317b37a031b31f636dd915c9
mucky <73165601+tarkanaciksoz@users.noreply.github.com>
2022-10-20 23:18:33 +0300

    workflow files to push image to aws-ecr

--- front-end/docker/test-and-build.sh
+++ front-end/docker/test-and-build.sh
@@ -5,7 +5,4 @@ cd app/ && npm run test:unit
 mv ./dist ../dist
 cd .. && rm -Rf app/
 mv dist app
-#mv dist ../ && cd .. && rm -rf app
-#mkdir app/ && cp -R dist/ app/ && rm -rf dist/
-#mv dist app
 nginx -g "daemon off;"
