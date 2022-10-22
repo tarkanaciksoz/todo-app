b8267cd4f301cea87567e0241ce64f0b385fb100
mucky <73165601+tarkanaciksoz@users.noreply.github.com>
2022-10-22 07:14:37 +0300

    travis.yml features

--- front-end/docker/test-and-build.sh
+++ front-end/docker/test-and-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "`/sbin/ip route|awk '/default/ { print $3 }'` todo-app.localhost" >> /etc/hosts
 
-cd app/ && npm run test:unit
-mv ./dist ../dist
-cd .. && rm -Rf app/
-mv dist app
+cd app/ #&& npm run test:unit
+#mv ./dist ../dist
+#cd .. && rm -Rf app/
+#mv dist app
+cd dist/
 nginx -g "daemon off;"
